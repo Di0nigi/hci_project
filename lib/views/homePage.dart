0ccd2b2ec0fae7cb8c440edfbe00c57e3072d2df
Dionigi <105047564+Di0nigi@users.noreply.github.com>
2024-04-17 16:51:35 +0200

    first commit

---
+++ lib/views/homePage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+     body: Container()
+    );
+  }
+}
