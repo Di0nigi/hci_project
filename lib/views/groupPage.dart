91fda04bf366b6912afd56b570d0edc4df826744
Dionigi <105047564+Di0nigi@users.noreply.github.com>
2024-06-06 11:34:16 +0200

    expense view first implementation still static

--- lib/views/groupPage.dart
+++ lib/views/groupPage.dart
@@ -9,7 +9,13 @@ class groupPage extends StatefulWidget {
   State<groupPage> createState() => _groupPageState();
 }
 
-var firstScreenContext;
+String arrowIcon = "assets\\arrowIcon.png";
+
+List<expenseContainer> expenseList = [
+  expenseContainer(title: "thai dinner", date: "20/2/24", author: "Billy", totalAmount: 25, yourAmount: 5)
+];
+
+Widget expensesView = updateExpenseview();
 
 newExpensePage newExp = newExpensePage();
 
@@ -66,14 +72,16 @@ class _groupPageState extends State<groupPage> {
               )
             ]),
             Container(
-              color: Color.fromARGB(0, 214, 0, 0),
+              color: Color.fromARGB(0, 0, 0, 0),
               width: width,
-          height: height - (height / 11) - (height / 10),
+              height: height - (height / 11) - (height / 10),
+              child: expensesView,
             ),
             Container(
-               width: width,
-            height: height / 11,
-              child: Row(mainAxisAlignment: MainAxisAlignment.center,
+              width: width,
+              height: height / 11,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
                     width: 75,
@@ -113,6 +121,93 @@ class _groupPageState extends State<groupPage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+Widget updateExpenseview() {
+  return ListView.builder(
+    itemCount: expenseList.length, // Replace with your data list length
+    itemBuilder: (BuildContext context, int index) {
+      //print(index);
+      return expenseList[index];
+    },
+  );
+}
+
+class expenseContainer extends StatelessWidget {
+  final String title;
+  final String date;
+  final String author;
+  final int totalAmount;
+  final int yourAmount;
+
+  expenseContainer(
+      {required this.title,
+      required this.date,
+      required this.author,
+      required this.totalAmount,
+      required this.yourAmount});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Padding(
+          padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+          child: Container(
+            height: 100,
+            decoration: BoxDecoration(
+                color: Color.fromARGB(0, 255, 0, 0),
+                border: Border.all(width: 3),
+                borderRadius: BorderRadius.circular(10)),
+            padding: EdgeInsets.fromLTRB(10, 5, 10, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      this.title,
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontFamily: 'impact',
+                          fontSize: 35),
+                    ),
+                    Padding(padding: EdgeInsets.all(2)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "payed by",
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontFamily: 'impact',
+                              fontSize: 30),
+                        ),
+                        Padding(padding: EdgeInsets.fromLTRB(50, 0, 0, 0)),
+                        Text(
+                          this.yourAmount.toString(),
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontFamily: 'impact',
+                              fontSize: 30),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                Container(
+                  width: 30,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(arrowIcon), fit: BoxFit.fitWidth)),
+                )
+              ],
+            ),
+          )),
+      onTap: () {
+        //Navigator.of(context).push(_createGroupRoute());
+      },
     );
   }
 }
